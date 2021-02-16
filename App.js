2ca89fa48db02f62d6ab37ac7a51f084da69c5d5
th7024 <th7024@gmail.com>
2021-02-17 01:22:57 +0900

    flex특성 정리

--- App.js
+++ App.js
@@ -5,17 +5,28 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.yellowView} />
+      <View style={styles.blueView} />
       <Text>Hello!!@!!</Text>
       <StatusBar style="auto" />
     </View>
   );
+  //컨테이너는 아버지 역할이고 yellow와 blue는 자식 느낌임
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // 모든 공간이 사용 가능하다.
+    //flex 레이아웃 의 초기상태는 세로형식임. 설정으로 바꿀 수 있음. flexDirection 으로 
+    //flex 값이 클 수록 레이아웃에서 더 많은 공간을 차지함
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: "blue"
+  }
+  
 });
