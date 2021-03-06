0f725a31849b3fc34b5a9a91039b3122c3d3ad91
th7024 <th7024@gmail.com>
2021-03-06 18:28:45 +0900

    날씨 만들기

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
-
-import React from 'react';
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { Alert } from 'react-native';
+import axios from "axios";
 
-export default class extends React.Component {3
+const API_KEY = "cb18f493cd8a9ccf630c35f5682d2f24";
+
+export default class extends React.Component {
   state = {
     isLoading: true
-  }
-  getLocation = async() => {
-    try{
-      await Location.requestPermissionsAsync();
-      const {coords: {latitude, longitude}
-    } = await Location.getCurrentPositionAsync();
-    this.setState({isLoading: false});
-      // API로 보내서 날씨 정보를 받아올거다
-      console.log(location);
-    }
-    catch(error){
-      Alert.alert("Can't find you.", "So sad")
-    }
-
   };
-  componentDidMount(){
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
     this.getLocation();
   }
-  render(){
-    const { isLoading} = this.state;
-   return isLoading? <Loading />: null;
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
-
